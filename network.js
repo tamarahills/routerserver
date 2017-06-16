bd09324770c308794155cb8c69fd4692e645804b
Tamara Hills <thills@mozilla.com>
2017-06-16 11:50:37 -0400

    Fixed problems with the hostnames not showing.  Added lan hostnames for src.  Filtered localhost and dupes.

--- network.js
+++ network.js
@@ -6,7 +6,6 @@
 const exec = require('child_process').exec;
 const fs = require('fs');
 const readline = require('readline');
-var ps = require('ps-node');
 var childProcess = require('child_process');
 const dns = require('dns');
 
@@ -26,60 +25,115 @@ function getReadableBytes(fileSizeInBytes) {
 NetworkConn.prototype.createProcess = function() {
   NetworkConn._retrieveChild = childProcess.fork('./reverse.js');
   NetworkConn._retrieveChild.on('message', function(msg) {
-    console.log('Inserting:'+ msg.dst + ':'+msg.host);
     NetworkConn.dnsCache.set(msg.dst, msg.host);
+    console.log('Caching:'+ msg.dst + ':'+msg.host + ': size:' + NetworkConn.dnsCache.size);
   }.bind(this));
+}
+
+function logMap(value, key, map) {
+  console.log(key + ' : ' + value);
+}
+
+function printCache() {
+  console.log('CACHE START===========');
+  NetworkConn.dnsCache.forEach(logMap);
+  console.log('CACHE END=============');
 }
 
 NetworkConn.prototype.addLanHost = function(ip, host) {
   NetworkConn.dnsCache.set(ip, host);
+  NetworkConn.lanHostnames.set(ip, host);
 }
 
 NetworkConn.prototype.getConnList = function(callback) {
+
   var data = {};
   data.connections = [];
+  var lineCount = 0;
 
+  console.log('===========NEW REQUEST==========: ' + data.connections.length);
   const rl = readline.createInterface({
     input: fs.createReadStream('/proc/net/nf_conntrack')
   });
 
   rl.on('line', function(line) {
+    lineCount++;
     var conn = {};
-    
+    console.log('line is: '+ line);    
     var arr = line.split(/\s+/);
+    var dstFound = false;
+    var srcFound = false;
+    var bytesFound = false;
+    var dport = 0;
+    var sport = 0;
     for(var key in arr) {
       var i = arr[key].indexOf('src=');
-      if(-1 != i) {
-        conn.src = arr[key].slice(i + 4);
+      if(-1 != i && !srcFound) {
+        var srcAddr = arr[key].slice(i + 4);
+	if (NetworkConn.lanHostnames.has(srcAddr)) {
+	  conn.src = NetworkConn.lanHostnames.get(srcAddr);
+	} else {
+          conn.src = srcAddr;
+	}
+	srcFound = true;
 	continue;
       }
 
       i = arr[key].indexOf('dst=');
-      if(-1 != i) {
+      if(-1 != i && !dstFound) {
         var dest = arr[key].slice(i + 4);
-	if (NetworkConn.dnsCache.has(conn.dst)) {
-	  var host = NetworkConn.dnsCache.get(conn.dst);
-	  console.log('VALUE CACHED:'+conn.dst + ':' + host);
+	dstFound = true;
+	if (NetworkConn.dnsCache.has(dest)) {
+	  var host = NetworkConn.dnsCache.get(dest);
 	  conn.dst = host;
 	} else {
 	  var msgData = { "dst": dest };
-	  console.log('SENDING DATA');
+	  console.log('CACHE MISS: ' + dest);
 	  NetworkConn._retrieveChild.send(msgData);
           conn.dst = dest;
+	  NetworkConn.dnsCache.set(dest, dest);
 	}
         continue;
       }
 
+      i = arr[key].indexOf('dport=')
+      if (-1 != i) {
+        var port = arr[key].slice(i+6);
+	dport = parseInt(port);
+	if(dport == 53) {
+	  break;
+	}
+      }
+
+      i = arr[key].indexOf('sport=')
+      if (-1 != i) {
+        var port = arr[key].slice(i+6);
+	sport = parseInt(port);
+	if (sport == 53) {
+	  break;
+	}
+      }
+
       i = arr[key].indexOf('bytes=');
-      if(-1 != i) {
+      if(-1 != i && !bytesFound) {
         conn.bytes = arr[key].slice(i + 6);
+	bytesFound = true;
 	continue;
       }
+      if(srcFound && dstFound && bytesFound) {
+        break;
+      }
     }
-    data.connections.push(conn);
+    
+    if (dport == 53 || sport == 53) {
+    } else if(conn.dst == '127.0.0.1' && conn.src == '127.0.0.1') {
+    } else {
+      console.log('ADDING:  ' + JSON.stringify(conn));
+      data.connections.push(conn);
+    }
   });
   rl.on('close', function() {
-    console.log('Close called');
+    console.log('Close called: ' + data.connections.length);
     rl.close();
     data.connections.sort(function(a, b) {
       return parseFloat(b.bytes) - parseFloat(a.bytes);
@@ -89,6 +143,8 @@ NetworkConn.prototype.getConnList = function(callback) {
       var pr = getReadableBytes(data.connections[key].bytes);
       data.connections[key].bytes = pr;
     }
+    console.log('==========RETURNING DATA=========: ' + data.connections.length);
+    console.log('==========FINAL LINECOUNT: ======: ' + lineCount);
     callback(data);
   });
 }
--- reverse.js
+++ reverse.js
@@ -2,24 +2,29 @@ const dns = require('dns');
 
 
 process.on('message', function(msg) {
-  console.log('RECEIVED MESSAGE IN CHILD:' + msg.dst);
-  dns.reverse(msg.dst, function(err, hostnames) {
-    if(err != null) {
-      console.log('Error resolving:' + err);
-      var data = {
-        "dst": msg.dst,
-	"host": msg.dst
-      };
-      process.send(data);
-    } else {
-      var data = {
-        "dst": msg.dst,
-        "host": hostnames[0]
-      };
-      console.log('RESOLVED.' + hostnames[0]);
-      process.send(data);
-    } 
-  });
+  if (msg.dst == '127.0.0.1') {
+    var data = {
+      "dst": msg.dst,
+      "host": 'localhost'
+    };
+    process.send(data);
+  } else {
+    dns.reverse(msg.dst, function(err, hostnames) {
+      if(err != null) {
+        var data = {
+          "dst": msg.dst,
+	  "host": msg.dst
+        };
+        process.send(data);
+      } else {
+        var data = {
+          "dst": msg.dst,
+          "host": hostnames[0]
+        };
+        process.send(data);
+      } 
+    });
+  }
 })
 
 process.on('uncaughtException', function(err) {
